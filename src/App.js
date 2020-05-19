b923851819ad487e0325feb1142c267982e18d59
Olayiwola Olanrewaju <olayiwolaolanrewaju@Olayiwolas-MacBook-Pro.local>
2020-05-19 15:39:56 +0100

    i don finsh am

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './App.css'
+
+const randomize = (req) => {
+  if (req === "num") {
+    return (
+      (Math.floor(Math.random() * 99))
+    )
+  }
+  else {
+    return (
+      `${new Date().getMonth() + 1}/${new Date().getDate()}/`
+    )
+  }
+}
+
+const display = (fact) => {
+  if (fact === null) {
+    return (
+      <div className="ui segment">
+        <div className="ui active dimmer">
+          <div className="ui text loader">Loading....</div>
+        </div>
+        <p></p>
+      </div>
+    )
+  }
+  else {
+    return (
+      fact
+    )
+  }
+}
 
 function App() {
+  const [state, setState] = useState('1')
+  const [fact, setFact] = useState('')
+
+  useEffect(() => {
+    setFact(null)
+    fetch(`http://numbersapi.com/${state}?json`)
+      .then(response => response.json())
+      .then(data => setFact(data.text))
+  }, [state])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-body">
+      <div className="top-div">
+        <section>{display(fact)}</section>
+      </div>
+      <div className="bottom-div">
+        <section>
+          <button onClick={() => setState(randomize("num"))}>Random Number</button>
+          <button onClick={() => setState(randomize("nu"))}>Today's Fact</button>
+        </section>
+        <section className="input">
+          <input type="number" name="num" placeholder="Enter a number you need facts" onChange={(e) => { setState(e.target.value) }} />
+        </section>
+      </div>
     </div>
   );
 }
